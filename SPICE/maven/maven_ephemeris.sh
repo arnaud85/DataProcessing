8a79f5bb73092979245b3ad9c55a1ab703a7e62d
Arnaud <arnaud.biegun@gmail.com>
2015-03-10 10:46:51 +0100

    clean

--- SPICE/maven/maven_ephemeris.sh
+++ SPICE/maven/maven_ephemeris.sh
@@ -8,16 +8,12 @@ clear
 gcc maven_ephemeris.c -o maven_ephemeris -I/home/arnaud/WORK/TOOLS/LIB/cspice/include -I/home/arnaud/WORK/TOOLS/INCLUDE /home/arnaud/WORK/TOOLS/LIB/cspice/lib/cspice.a -L/home/arnaud/WORK/TOOLS/LIB /home/arnaud/WORK/TOOLS/LIB/libnetcdf.so.7 -lDD -lm
 
 # Download last version of MAVEN ephemride kernel
-# wget http://naif.jpl.nasa.gov/pub/naif/MAVEN/kernels/spk/maven_orb.bsp -P kernels/Maven/
-# wget http://naif.jpl.nasa.gov/pub/naif/MAVEN/kernels/spk/trj_orb_00777-00779_00939_v1.bsp -P kernels/Maven/
 # wget http://naif.jpl.nasa.gov/pub/naif/MAVEN/kernels/spk/maven_orb_rec.bsp -P kernels/Maven
-# wget http://naif.jpl.nasa.gov/pub/naif/MAVEN/kernels/spk/mar097s.bsp -P kernels/general
-# wget http://naif.jpl.nasa.gov/pub/naif/MAVEN/kernels/fk/maven_v05.tf -P kernels/Maven
+# mv kernels/Maven/maven_orb_rec.bsp.1 kernels/Maven/maven_orb_rec.bsp
 
 #Execute 
-# ./maven_ephemeris "MAVEN" "MARS" "maven" "2014 DEC 01 00:00:00 UTC" "2014 DEC 02 01:00:00 UTC"
-./maven_ephemeris "MAVEN" "MARS" "maven" "2014 DEC 31 00:00:00 UTC" "2015 JAN 02 01:00:00 UTC"	
-
+# ./maven_ephemeris "MAVEN" "MARS" "maven" "2014 SEP 23 13:03:46 UTC" "2015 MAR 03 16:08:21 UTC"	2015
+./maven_ephemeris "MAVEN" "MARS" "maven" "2014 SEP 23 13:03:46 UTC" "2015 JAN 01 00:01:00 UTC"
 
 #Move to nc directory
 mv *.txt output/plot/
